545a72c254fd3b54653b16c9506f36f848addd41
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2025-02-07 21:29:20 -0300

    implementando datastore

--- app/src/main/java/br/edu/ifsp/dmo1/projeto_disciplina_dmo1/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/projeto_disciplina_dmo1/ui/main/MainActivity.kt
@@ -2,8 +2,10 @@ package br.edu.ifsp.dmo1.projeto_disciplina_dmo1.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.edu.ifsp.dmo1.projeto_disciplina_dmo1.databinding.ActivityMainBinding
 import br.edu.ifsp.dmo1.projeto_disciplina_dmo1.ui.aluno.AlunoActivity
@@ -24,7 +26,30 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        binding.checkboxManterLogado.visibility = View.INVISIBLE
+
         configListeners()
+        setupObservers()
+
+    }
+
+    private fun setupObservers(){
+        viewModel.checkboxPreferences.observe(this, Observer {
+            val (salvarDados, manterLogado) = it
+
+            binding.checkboxSalvarDados.isChecked = salvarDados
+            binding.checkboxManterLogado.isChecked = manterLogado
+        })
+
+        viewModel.dadosPreferences.observe(this, Observer {
+            val(email, senha) = it
+
+            binding.emailEditText.setText(email)
+            if(email.isNotEmpty()){
+                binding.senhaEditText.setText(senha)
+            }
+        })
+
     }
 
     private fun configListeners(){
@@ -40,11 +65,21 @@ class MainActivity : AppCompatActivity() {
         val email = binding.emailEditText.text.toString()
         val senha = binding.senhaEditText.text.toString()
 
+        val salvarDados = binding.checkboxSalvarDados.isChecked
+        val manterLogado = binding.checkboxManterLogado.isChecked
+
+        binding.emailEditText.setText("")
+        binding.senhaEditText.setText("")
+        binding.checkboxSalvarDados.isChecked = false
+        binding.checkboxManterLogado.isChecked = false
+
         if (email.isNotEmpty() && senha.isNotEmpty()) {
             viewModel.getUsuarioByEmail(email) { usuario ->
                 if (usuario != null && usuario.senha == senha && usuario.email == email) {
                     Toast.makeText(this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT)
                         .show()
+
+                    viewModel.prefereces(email, senha, salvarDados, manterLogado)
 
                     if (usuario.tipoUsuario.equals("PROFESSOR")){
                         val intent = Intent(this, ProfessorActivity::class.java)
--- app/src/main/java/br/edu/ifsp/dmo1/projeto_disciplina_dmo1/ui/main/MainViewModel.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/projeto_disciplina_dmo1/ui/main/MainViewModel.kt
@@ -4,19 +4,38 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import br.edu.ifsp.dmo1.projeto_disciplina_dmo1.data.model.Usuario
+import br.edu.ifsp.dmo1.projeto_disciplina_dmo1.data.repository.DataStoreRepository
 import br.edu.ifsp.dmo1.projeto_disciplina_dmo1.data.repository.UsuarioRepository
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
 
     private val repository = UsuarioRepository(application)
+    private val datastore = DataStoreRepository(application)
+
+    val checkboxPreferences: LiveData<Pair<Boolean, Boolean>> = datastore.checkboxPreferences.asLiveData()
+    val dadosPreferences: LiveData<Pair<String, String>> = datastore.dadosPreferences.asLiveData()
 
     fun getUsuarioByEmail(email: String, onResult: (Usuario?) -> Unit) = viewModelScope.launch {
         val usuario = repository.getUsuarioByEmail(email)
         onResult(usuario)
     }
 
+    fun prefereces(email: String, senha: String, salvarDados: Boolean, manterLogado: Boolean){
+        if(salvarDados || manterLogado){
+            savePreferences(email, senha, salvarDados, manterLogado)
+        }else{
+            savePreferences("", "", salvarDados, manterLogado)
+        }
+    }
+
+    private fun savePreferences(email: String, senha: String, salvarDados: Boolean, manterLogado: Boolean) {
+        viewModelScope.launch {
+            datastore.savePreferences(email, senha, salvarDados, manterLogado)
+        }
+    }
 
 }
